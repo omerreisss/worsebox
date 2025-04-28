a0a85a700287f239b8c88f4713da48aca7c823a5
omerreisss <comertomerizzet@gmail.com>
2025-04-28 16:50:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Worsebox Başarılı Şekilde Çalışıyor!');
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu çalışıyor: http://localhost:${port}`);
+});
